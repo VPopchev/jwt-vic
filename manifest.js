cd687214f15d611fb11dc17eed91b99050b737fb
Victor Popchev <victor.popchev@wpxhosting.com>
2020-08-18 12:47:03 +0300

    changes in manifest.js

--- manifest.js
+++ manifest.js
@@ -1,7 +1,7 @@
 module.exports = {
     version: "0.0.1",
     init: function(pluginContext) {
-        let policy = require('./jwt/jwt')
+        let policy = require('./jwt/index')
 		pluginContext.registerPolicy(policy)
     }
 }
